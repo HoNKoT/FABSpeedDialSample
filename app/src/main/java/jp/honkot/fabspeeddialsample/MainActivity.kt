a9dc291d9cca844bd01ddeab1b891a260f8bfca0
Hiroki Honda <hon87kot12.xp@gmail.com>
2019-10-24 16:38:10 +0900

    add plus fab with svg icon

--- app/src/main/java/jp/honkot/fabspeeddialsample/MainActivity.kt
+++ app/src/main/java/jp/honkot/fabspeeddialsample/MainActivity.kt
@@ -1,12 +1,26 @@
 package jp.honkot.fabspeeddialsample
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.res.ResourcesCompat
+import androidx.databinding.DataBindingUtil
+import com.leinardi.android.speeddial.SpeedDialActionItem
+import jp.honkot.fabspeeddialsample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+//        binding.speedDialView.addActionItem(
+//            SpeedDialActionItem.Builder(R.id.fab_custom_color, drawable)
+//            .setFabBackgroundColor(ResourcesCompat.getColor(getResources(), R.color.material_white_1000, getTheme()))
+//            .setFabImageTintColor(ResourcesCompat.getColor(getResources(), R.color.inbox_primary, getTheme()))
+//            .setLabel(getString(R.string.label_custom_color))
+//            .setLabelColor(Color.WHITE)
+//            .setLabelBackgroundColor(ResourcesCompat.getColor(getResources(), R.color.inbox_primary, getTheme()))
+//            .setLabelClickable(false)
+//            .create())
     }
 }
