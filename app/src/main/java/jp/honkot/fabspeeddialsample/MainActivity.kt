762bc04bc23298f4e09c1301797cff704aa1c1d7
Hiroki Honda <hon87kot12.xp@gmail.com>
2019-10-24 16:51:05 +0900

    add action listener

--- app/src/main/java/jp/honkot/fabspeeddialsample/MainActivity.kt
+++ app/src/main/java/jp/honkot/fabspeeddialsample/MainActivity.kt
@@ -3,17 +3,54 @@ package jp.honkot.fabspeeddialsample
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.core.content.res.ResourcesCompat
 import androidx.databinding.DataBindingUtil
 import com.leinardi.android.speeddial.SpeedDialActionItem
+import com.leinardi.android.speeddial.SpeedDialView
 import jp.honkot.fabspeeddialsample.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SpeedDialView.OnActionSelectedListener {
+
+    private val binding: ActivityMainBinding by lazy {
+        DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding =
-            DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
-        binding.speedDial.inflate(R.menu.menu_fab_speed_dial)
+        binding.speedDial.run {
+            inflate(R.menu.menu_fab_speed_dial)
+            setOnActionSelectedListener(this@MainActivity)
+        }
+
+    }
+
+    override fun onActionSelected(actionItem: SpeedDialActionItem?): Boolean {
+        val ret = when (actionItem?.id) {
+            R.id.action_menu_1 -> {
+                Toast.makeText(this, "this is menu 1", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.action_menu_2 -> {
+                Toast.makeText(this, "this is menu 2", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.action_menu_3 -> {
+                Toast.makeText(this, "this is menu 3", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.action_menu_4 -> {
+                Toast.makeText(this, "this is menu 4", Toast.LENGTH_SHORT).show()
+                true
+            }
+
+            else -> false
+        }
+
+        if (ret) {
+            binding.speedDial.close()
+        }
+
+        return ret
     }
 }
