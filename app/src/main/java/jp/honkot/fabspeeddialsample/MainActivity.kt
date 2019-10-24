a5dfcdd6326caa62a7efec987bd17b4cb4980199
Hiroki Honda <hon87kot12.xp@gmail.com>
2019-10-24 16:44:30 +0900

    add speed dial by menu.xml

--- app/src/main/java/jp/honkot/fabspeeddialsample/MainActivity.kt
+++ app/src/main/java/jp/honkot/fabspeeddialsample/MainActivity.kt
@@ -12,15 +12,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
-//        binding.speedDialView.addActionItem(
-//            SpeedDialActionItem.Builder(R.id.fab_custom_color, drawable)
-//            .setFabBackgroundColor(ResourcesCompat.getColor(getResources(), R.color.material_white_1000, getTheme()))
-//            .setFabImageTintColor(ResourcesCompat.getColor(getResources(), R.color.inbox_primary, getTheme()))
-//            .setLabel(getString(R.string.label_custom_color))
-//            .setLabelColor(Color.WHITE)
-//            .setLabelBackgroundColor(ResourcesCompat.getColor(getResources(), R.color.inbox_primary, getTheme()))
-//            .setLabelClickable(false)
-//            .create())
+        val binding =
+            DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        binding.speedDial.inflate(R.menu.menu_fab_speed_dial)
     }
 }
